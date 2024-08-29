a1f09b70d445fdb68c111d9ec97d91376b85a2bc
Tatiana Yoshida <tatyoshida@gmail.com>
2024-08-29 13:28:39 -0300

    script-js added

---
+++ script.js
@@ -0,0 +1,18 @@
+var second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+var countDown = new Date("Sep 21, 2024 00:00").getTime(),
+  x = setInterval(function () {
+    var now = new Date().getTime(),
+      distance = countDown - now;
+
+    (document.getElementById("days").innerText = Math.floor(distance / day)),
+      (document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+      )),
+      (document.getElementById("minutes").innerText = Math.floor(
+        (distance % hour) / minute
+      ));
+}, second);
